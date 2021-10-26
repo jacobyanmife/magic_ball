825936aa839e4ccbd14c521944caae30b8cf9856
jacobyanmife <jacobyanmife@gmail.com>
2021-10-26 14:31:35 +0100

    magic ball

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.indigo,
+          title: Text('Ask Me Anything'),
+        ),
+        body: MagicBall(),
+      ),
+    ),
+  );
+}
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ChangeBallNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  ChangeBallNumber= Random().nextInt(5)+1;
+                });
+              },
+              child: Image.asset('images/ball$ChangeBallNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
